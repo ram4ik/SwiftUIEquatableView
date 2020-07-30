6d16cae113addb4149e0195ad685d843587a1dd6
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-30 16:22:08 +0300

    Initial Commit

---
+++ SwiftUIEquatableView/SwiftUIEquatableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEquatableViewApp.swift
+//  SwiftUIEquatableView
+//
+//  Created by ramil on 30.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEquatableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
